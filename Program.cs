d14292d486f2746208bb0807b8b86a502abcc108
Emil lindgren <hoganas_1937_70@hotmail.com>
2016-02-23 13:56:48 +0100

    logger.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            var myLogger = new Logger();
+            myLogger.Log("First post");
+            myLogger.Log("Second post");
+            myLogger.Log("another post");
+            foreach (var item in myLogger.LogPosts)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
